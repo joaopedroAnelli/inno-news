826558cdd1c5996ec40b766b6b7a88172a75b1a1
joaopedroAnelli <joaopedroanelli99@gmail.com>
2023-07-17 08:32:25 -0300

    Update service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,13 +2,40 @@ self.addEventListener('push', function (event) {
   console.log('Received a push message', event);
 
   var title = 'Inno News';
-  var body = event.data?.text();
+  var body = event.data?.json();
+
+  var { text } = body;
+
   var icon = '/icon.png';
 
   event.waitUntil(
     self.registration.showNotification(title, {
-      body,
+      text,
       icon,
+      actions: [
+        {
+          action: 'open',
+          title: 'Ver Artigo',
+          icon: '/artivle.svg',
+        },
+      ],
     })
   );
+});
+
+self.addEventListener('notificationclick', function (event) {
+  console.log('On notification click: ', event.notification);
+  event.notification.close();
+
+  var body = event.notification.data?.json();
+
+  console.log('body', body);
+
+  var { link } = body;
+
+  console.log('link', link);
+
+  if (event.action === 'open') {
+    clients.openWindow(link);
+  }
 });
